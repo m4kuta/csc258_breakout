0a771468513a362273210b7fb510aafe901b9790
haolin <haolinf@gmail.com>
2022-11-26 16:15:16 -0500

    Initial commit

---
+++ breakout.asm
@@ -0,0 +1,48 @@
+################ CSC258H1F Fall 2022 Assembly Final Project ##################
+# This file contains our implementation of Breakout.
+#
+# Student 1: Name, Student Number
+# Student 2: Haolin Fan, 1003364316
+######################## Bitmap Display Configuration ########################
+# - Unit width in pixels:       TODO
+# - Unit height in pixels:      TODO
+# - Display width in pixels:    TODO
+# - Display height in pixels:   TODO
+# - Base Address for Display:   0x10008000 ($gp)
+##############################################################################
+
+    .data
+##############################################################################
+# Immutable Data
+##############################################################################
+# The address of the bitmap display. Don't forget to connect it!
+ADDR_DSPL:
+    .word 0x10008000
+# The address of the keyboard. Don't forget to connect it!
+ADDR_KBRD:
+    .word 0xffff0000
+
+##############################################################################
+# Mutable Data
+##############################################################################
+
+##############################################################################
+# Code
+##############################################################################
+	.text
+	.globl main
+
+	# Run the Brick Breaker game.
+main:
+    # Initialize the game
+
+game_loop:
+	# 1a. Check if key has been pressed
+    # 1b. Check which key has been pressed
+    # 2a. Check for collisions
+	# 2b. Update locations (paddle, ball)
+	# 3. Draw the screen
+	# 4. Sleep
+
+    #5. Go back to 1
+    b game_loop
